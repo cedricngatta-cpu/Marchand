a5fcffbb680b4b6129f2f9bb9793b6f670cb3ff9
cedricngatta-cpu <cedric.ngatta@groupecerco.com>
2026-03-05 17:01:32 +0000

    fix(sw): add webworker type reference to fix build error

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,9 +1,10 @@
+/// <reference lib="webworker" />
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
 
 declare global {
-    interface WorkerGlobalScope extends SerwistGlobalConfig {
+    interface ServiceWorkerGlobalScope extends SerwistGlobalConfig {
         __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
     }
 }
